e709ab23e877e199527cecfec3e5c5aaa901da71
hungdz2k1 <nguyenphuonghung1002@gmail.com>
2024-07-08 16:22:50 +0700

    update

---
+++ sessiom14.js
@@ -0,0 +1,31 @@
+
+// let n = +(prompt("Moi ban nhap vao 1 so"));
+
+// let count = 0;
+
+// let num = 2;
+
+
+// while(count < n) {
+
+//    let check = true;
+
+//     for (let index = 2; index < num; index++) {
+
+//         if(num % index === 0) {
+//              check = false;
+//         }
+//     }
+
+//     if(check) {
+//       console.log(num);
+//       count++;
+//     }
+
+//   num++;
+
+// }
+
+
+
+// 
